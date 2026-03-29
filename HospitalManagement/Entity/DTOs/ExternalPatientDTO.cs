9ea8fb17575cdd72df6ef6c50f36d3d9a06c3609
AndreeaJigarov <andreea.jigarov@stud.ubbcluj.ro>
2026-03-29 16:55:27 +0300

    feat- added RecordDTO and ExternalPatientDTO

---
+++ HospitalManagement/Entity/DTOs/ExternalPatientDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HospitalManagement.Entity.Enums;
+
+namespace HospitalManagement.Entity.DTOs
+{
+    public class ExternalPatientDTO
+    {
+        public string CNP { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public Sex Sex { get; set; }
+        public DateTime EmergencyTimestamp { get; set; }
+        public string? Injury { get; set; }
+    }
+}
