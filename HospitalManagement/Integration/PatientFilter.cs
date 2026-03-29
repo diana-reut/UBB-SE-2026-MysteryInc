e36a00e6f4cd240ee495a21d87aa408f064b2e9e
AmaliaHaprian <amalia.haprian@stud.ubbcluj.ro>
2026-03-29 10:53:26 +0300

    feat: added PatientFilter class to optimize patient filtering

---
+++ HospitalManagement/Integration/PatientFilter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HospitalManagement.Entity.Enums;
+
+namespace HospitalManagement.Integration
+{
+    class PatientFilter
+    {
+        public string? namePart;
+        public string? CNP;
+        public int? minAge;
+        public int? maxAge;
+        public Sex? sex;
+        public bool? hasChronicCond;
+        public DateTime? lastUpdatedFrom;
+        public DateTime? lastUpdatedTo;
+        public BloodType? bloodType;
+        public RhEnum? rh;
+    }
+}
