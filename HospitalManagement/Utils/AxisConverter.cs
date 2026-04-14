26784851cd76c1acd885dabef4a135a5e05c07a8
diana-reut <dianaradauti2013@gmail.com>
2026-04-14 10:09:36 +0300

    utils

--- HospitalManagement/Utils/AxisConverter.cs
+++ HospitalManagement/Utils/AxisConverter.cs
@@ -1,27 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
 using Microsoft.UI.Xaml.Data;
 using LiveChartsCore.SkiaSharpView;
-using LiveChartsCore.Kernel.Sketches;
 
-namespace HospitalManagement.Utils
+namespace HospitalManagement.Utils;
+
+public class AxisConverter : IValueConverter
 {
-    public class AxisConverter : IValueConverter
+    public object? Convert(object value, Type targetType, object parameter, string language)
     {
-        public object Convert(object value, Type targetType, object parameter, string language)
+        // Assumes value is Axis[] and needs to be IEnumerable<ICartesianAxis>
+        if (value is Axis[] axes)
         {
-            // Assumes value is Axis[] and needs to be IEnumerable<ICartesianAxis>
-            if (value is Axis[] axes)
-            {
-                // Axis implements ICartesianAxis, so cast is safe
-                return axes as IEnumerable<ICartesianAxis>;
-            }
-            return null;
+            // Axis implements ICartesianAxis, so cast is safe
+            return axes;
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, string language)
-        {
-            throw new NotImplementedException();
-        }
+        return null;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new MyNotImplementedException();
     }
 }
