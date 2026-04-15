41a0a02870166a7d9b74942ea0c2cb93a2fa2d89
diana-reut <dianaradauti2013@gmail.com>
2026-04-15 09:53:53 +0300

    prescription service

--- HospitalManagement/Service/PrescriptionService.cs
+++ HospitalManagement/Service/PrescriptionService.cs
@@ -5,61 +5,55 @@ using HospitalManagement.Entity;
 using HospitalManagement.Repository;
 using HospitalManagement.Integration;
 
-namespace HospitalManagement.Service
+namespace HospitalManagement.Service;
+
+public class PrescriptionService
 {
-    public class PrescriptionService
+    private readonly PrescriptionRepository _prescriptionRepository;
+
+    public PrescriptionService(PrescriptionRepository prescriptionRepository)
     {
-        private readonly PrescriptionRepository _prescriptionRepository;
+        _prescriptionRepository = prescriptionRepository ?? throw new ArgumentNullException(nameof(prescriptionRepository));
+    }
 
-        public PrescriptionService(PrescriptionRepository prescriptionRepository)
+
+    // <param name="n">Number of items per page</param>
+    // <param name="page">Page number (starting from 1)</param>
+    // <returns>List of Prescriptions</returns>
+    public List<Prescription> GetLatestPrescriptions(int n, int page)
+    {
+        return _prescriptionRepository.GetTopN(n, page);
+    }
+
+
+    // <param name="id">The ID of the prescription</param>
+    // <returns>The specified Prescription</returns>
+    public Prescription GetPrescriptionDetails(int id)
+    {
+        var filter = new PrescriptionFilter { PrescriptionId = id, };
+        Prescription? prescription = _prescriptionRepository.GetFiltered(filter).FirstOrDefault() ?? throw new ArgumentException($"Prescription with ID {id} does not exist.");
+
+        return prescription;
+    }
+
+    // <param name="filter">The complex filter for finding prescriptions</param>
+    // <returns>A filtered list of Prescriptions sorted by Date descending.</returns>
+    public List<Prescription> ApplyFilter(PrescriptionFilter filter)
+    {
+        if (filter is null)
         {
-            _prescriptionRepository = prescriptionRepository ?? throw new ArgumentNullException(nameof(prescriptionRepository));
+            return _prescriptionRepository.GetTopN(20, 1);
         }
 
-     
-        /// <param name="n">Number of items per page</param>
-        /// <param name="page">Page number (starting from 1)</param>
-        /// <returns>List of Prescriptions</returns>
-        public List<Prescription> GetLatestPrescriptions(int n, int page)
+        try
         {
-            return _prescriptionRepository.GetTopN(n, page);
+            List<Prescription> results = _prescriptionRepository.GetFiltered(filter);
+
+            return results;
         }
-
-      
-        /// <param name="id">The ID of the prescription</param>
-        /// <returns>The specified Prescription</returns>
-        public Prescription GetPrescriptionDetails(int id)
+        catch (Exception)
         {
-            var filter = new PrescriptionFilter { PrescriptionId = id };
-            var prescription = _prescriptionRepository.GetFiltered(filter).FirstOrDefault();
-
-            if (prescription == null)
-            {
-                throw new Exception($"Prescription with ID {id} does not exist.");
-            }
-
-            return prescription;
-        }
-
-        /// <param name="filter">The complex filter for finding prescriptions</param>
-        /// <returns>A filtered list of Prescriptions sorted by Date descending.</returns>
-        public List<Prescription> ApplyFilter(PrescriptionFilter filter)
-        {
-            if (filter == null)
-            {
-                return _prescriptionRepository.GetTopN(20, 1);
-            }
-
-            try
-            {
-                var results = _prescriptionRepository.GetFiltered(filter);
-
-                return results;
-            }
-            catch (Exception)
-            {
-                throw new Exception("The medication search could not be completed at this time due to high system load or complex parameters. Please try simplifying your search or try again later.");
-            }
+            throw new MyNotImplementedException("The medication search could not be completed at this time due to high system load or complex parameters. Please try simplifying your search or try again later.");
         }
     }
 }
