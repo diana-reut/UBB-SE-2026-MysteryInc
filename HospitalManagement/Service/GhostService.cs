2718c21c2c967138ece77fc8d2856fb22b424db7
diana-reut <dianaradauti2013@gmail.com>
2026-04-15 09:25:41 +0300

    ghost service

--- HospitalManagement/Service/GhostService.cs
+++ HospitalManagement/Service/GhostService.cs
@@ -1,37 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace HospitalManagement.Service
+namespace HospitalManagement.Service;
+
+public class GhostService
 {
-    public class GhostService
+    private static readonly GhostService ServiceInstance = new();
+
+    public static GhostService Instance => ServiceInstance;
+
+    private GhostService()
     {
-        private static readonly GhostService _instance = new GhostService();
-        public static GhostService Instance => _instance;
-        private GhostService() { }
+    }
 
-        private readonly List<DateTime> _sightings = new();
-        public event EventHandler? ExorcismTriggered;
+    private readonly List<DateTime> _sightings = [];
 
-        public void SawAGhost()
+    public event EventHandler? ExorcismTriggered;
+
+    public void SawAGhost()
+    {
+        CleanOldSightings();
+        _sightings.Add(DateTime.Now);
+        if (IsExorcismTriggered())
         {
-            CleanOldSightings();
-            _sightings.Add(DateTime.Now);
-            if (IsExorcismTriggered())
-                ExorcismTriggered?.Invoke(this, EventArgs.Empty);
+            ExorcismTriggered?.Invoke(this, EventArgs.Empty);
         }
+    }
 
-        public bool IsExorcismTriggered()
-        {
-            CleanOldSightings();
-            return _sightings.Count > 3;
-        }
+    public bool IsExorcismTriggered()
+    {
+        CleanOldSightings();
+        return _sightings.Count > 3;
+    }
 
-        private void CleanOldSightings()
-        {
-            _sightings.RemoveAll(s => s < DateTime.Now.AddHours(-24));
-        }
+    private void CleanOldSightings()
+    {
+        _ = _sightings.RemoveAll(s => s < DateTime.Now.AddHours(-24));
     }
 }
