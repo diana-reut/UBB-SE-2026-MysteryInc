6bd6c9c401bafe41f41a1ac91fae7d7d89a9ce0d
Mekk13 <mariasuciu313@gmail.com>
2026-04-21 14:47:50 +0300

    folders and general structure for tests

---
+++ HospitalManagement/Tests/IntegrationTests/PatientRepositoryIntegrationTests.cs
@@ -0,0 +1,45 @@
+﻿using HospitalManagement.Database;
+using HospitalManagement.Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+using HospitalManagement.Configuration;
+
+namespace HospitalManagement.Tests.IntegrationTests;
+
+[TestClass]
+internal class PatientRepositoryIntegrationTests
+{
+    private IDbContext _context;
+    private IPatientRepository _repo;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        string json = File.ReadAllText("testconfig.local.json");
+        string connStr = JsonSerializer.Deserialize<JsonElement>(json)
+                            .GetProperty("ConnectionStrings")
+                            .GetProperty("DefaultConnection")
+                            .GetString()!;
+
+        typeof(Config)
+            .GetProperty("ConnectionString")!
+            .SetValue(null, connStr);
+
+
+        _context = new HospitalDbContext();
+        _repo = new PatientRepository(_context);
+        
+    }
+
+    [TestCleanup]
+    public void Cleanup()
+    {
+        (_context as IDisposable)?.Dispose();
+    }
+}
