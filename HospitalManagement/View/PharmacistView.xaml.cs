82c040d5da06585d26d24650aaa7698ae42c5fee
Mekk13 <mariasuciu313@gmail.com>
2026-04-28 23:53:34 +0300

    Moved logic out of PharmacistView into PharmacistViewModel

--- HospitalManagement/View/PharmacistView.xaml.cs
+++ HospitalManagement/View/PharmacistView.xaml.cs
@@ -6,18 +6,28 @@ using System;
 
 namespace HospitalManagement.View;
 
-internal sealed partial class PharmacistView : Window
+public sealed partial class PharmacistView : Window
 {
     public PharmacistViewModel ViewModel { get; }
 
-    private readonly IServiceProvider _services;
-
     public PharmacistView()
     {
-        ViewModel = (App.Current as App).Services.GetService<PharmacistViewModel>();
-        InitializeComponent();
-        _services = (App.Current as App).Services;
+        ViewModel = (Application.Current as App)!.Services.GetRequiredService<PharmacistViewModel>();
 
+        InitializeComponent();
+
+        if (this.Content is FrameworkElement root)
+        {
+            root.DataContext = ViewModel;
+        }
+
+        MaximizeWindow();
+
+        ViewModel.RequestClose += () => this.Close();
+    }
+
+    private void MaximizeWindow()
+    {
         nint hwnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
         Microsoft.UI.WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hwnd);
         var appWindow = AppWindow.GetFromWindowId(windowId);
@@ -25,65 +35,10 @@ internal sealed partial class PharmacistView : Window
         {
             presenter.Maximize();
         }
-
-        if (ViewModel is null)
-        {
-            throw new InvalidOperationException("PharmacistViewModel not found in service provider.");
-        }
-
-        ViewModel.PropertyChanged += ViewModel_PropertyChanged;
-
-        if (Content is FrameworkElement rootElement)
-        {
-            rootElement.DataContext = ViewModel;
-        }
-
-        if (ViewModel.CurrentView is null)
-        {
-            throw new InvalidOperationException("PharmacistViewModel not found in service provider.");
-        }
-
-        UpdateView(ViewModel.CurrentView);
-    }
-
-    private void ViewModel_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
-    {
-        if (e.PropertyName == nameof(PharmacistViewModel.CurrentView))
-        {
-            if (ViewModel.CurrentView is null)
-            {
-                throw new InvalidOperationException("PharmacistViewModel not found in service provider.");
-            }
-
-            UpdateView(ViewModel.CurrentView);
-        }
-    }
-
-    private void UpdateView(string viewName)
-    {
-        switch (viewName)
-        {
-            case "Prescriptions":
-            {
-                PrescriptionView prescriptionView = _services.GetRequiredService<PrescriptionView>();
-                MainContentArea.Content = prescriptionView;
-                break;
-            }
-            case "Addicts":
-            {
-                AddictView addictView = _services.GetRequiredService<AddictView>();
-                MainContentArea.Content = addictView;
-                break;
-            }
-            default:
-                break;
-        }
     }
 
     private void BackToHomeButton_Click(object sender, RoutedEventArgs e)
     {
-        var mainWindow = new MainWindow();
-        mainWindow.Activate();
-        Close();
+        ViewModel.NavigateBackToHome();
     }
 }
